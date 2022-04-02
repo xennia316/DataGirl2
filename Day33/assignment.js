61625120cb056b132c312007fe105ff141953ef1
xennia316 <xennia316@gmail.com>
2022-04-02 18:26:00 +0100

    assignment

---
+++ Day33/assignment.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+const names = [
+    { id: 1, name: 'Yaya', age: 24 },
+    { id: 2, name: 'Erica', age: 23 },
+    { id: 3, name: 'Sonia', age: 22 },
+    { id: 4, name: 'Eddy', age: 20 },
+    { id: 5, name: 'Okay', age: 04 },
+    { id: 6, name: 'Telli', age: 42 },
+];
+app.listen(8000, (req, res) => {
+
+});
+
+app.get("/users", (req, res) => {
+    res.json(names);
+});
+
+app.get("/users/:id", (req, res) => {
+    const found = names.filter((guy) => guy.id === parseInt(req.params.id))
+    res.json(found)
+});
+
+app.get("users/delete/:id", (req, res) => {
+    
+})
