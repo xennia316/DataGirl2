5fde64d2994c910b90251322174a1705c7f9a8e9
xennia316 <xennia316@gmail.com>
2022-04-02 18:25:38 +0100

    main server

---
+++ Day33/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const nothing = [
+    {
+        id: 1,
+        name: 'lll',
+        age: 12
+    },
+    {
+        id: 2,
+        name: 'mmm',
+        age: 14
+    },
+    {
+        id: 3,
+        name: 'dez',
+        age: 19
+    },
+]
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Get Request")
+})
+
+app.get("/nothing", (req, res)=>{
+    res.json(nothing);
+})
+
+app.get("/delete/:id", (req, res) => {
+    res.json(nothing.filter((member) => member.id === parseInt(req.params.id)));
+})
+
+app.listen(4000, () => {
+    console.log('Heyyy');
+})
