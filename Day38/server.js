e6a753029018c6a6861c5e06952abd514a13a248
xennia316 <xennia316@gmail.com>
2022-04-12 20:04:19 +0100

    hosting server

---
+++ Day38/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const UserRouter =  require('./Routes/user.router')
+
+const mongoose =  require('mongoose');
+
+const connection = mongoose.connect("mongodb+srv://Sonia:ZENNIA0709@cluster0.hcmtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+connection.then(() => {
+  console.log("The connection was succesful.");
+})
+
+connection.catch(() => {
+    console.log("Connection was not succesful.");
+})
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/api/user/', UserRouter)
+
+app.listen(5000)
