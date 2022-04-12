4eb6f06ba1a63d4aed87567964ee5c8fe615eefe
xennia316 <xennia316@gmail.com>
2022-04-12 20:04:51 +0100

    user route

---
+++ Day38/Routes/user.router.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const UserModel = require('../Models/User.model')
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/create_account', async (req, res) => {
+    const body = req.body;
+    const user = await new UserModel(body);
+    console.log(user);
+    await user.save();
+    return res.json({ message: ' User created successfully', data: user });
+});
+
+router.post('/login', async (req, res) => {
+    const { number, password } = req.body
+
+    const isUserFound = await UserModel.findOne({ number: number, password: password })
+    res.json(
+        isUserFound ? { message: 'user found', user: isUserFound } : { message: "User not found" }
+    )
+})
+
+module.exports = router
